a604b644b4bea1c3539efda76ab18d07cab4a5b2
Givi Gvinianidze <gvinianidzegivi@gmail.com>
2026-09-10 21:40:21 +0400

    client: add routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,40 @@
-import "./App.css";
+import { useState } from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router";
+import { Create } from "./pages/Create";
+import { Tasks } from "./pages/Tasks";
+import { Welcome } from "./pages/Welcome";
 
 export const App = () => {
-  return <main>App</main>;
+  return (
+    <BrowserRouter>
+      <Content />
+    </BrowserRouter>
+  );
+}
+
+const Content = () => {
+  const location = useLocation();
+  const [displayLocation, setDisplayLocation] = useState(location);
+  const [transitionStage, setTransistionStage] = useState("fadeIn");
+
+
+  if (location !== displayLocation) setTransistionStage("fadeOut");
+
+  return (
+    <div
+      className={transitionStage}
+      onAnimationEnd={() => {
+        if (transitionStage === "fadeOut") {
+          setDisplayLocation(location);
+          setTransistionStage("fadeIn");
+        }
+      }}
+    >
+      <Routes location={displayLocation}>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/tasks" element={<Tasks />} />
+      </Routes>
+    </div>
+  );
 };
